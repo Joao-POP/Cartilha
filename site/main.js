9c16a41e12813e7e01c43b49a5337955b1bcd7e9
Joao-POP <joaopap@proton.me>
2026-08-04 19:33:26 -0300

    Did styling
    - Added new page layout
    - An information dropdown
    - Links
    - A video player
    - Logos

--- site/main.js
+++ site/main.js
@@ -1,1 +1,9 @@
 console.log("Olá, Mundo!");
+
+const infos = document.getElementsByClassName("info");
+
+for (const info of infos) {
+    info.addEventListener("click", () => {
+        info.classList.toggle("open");
+    });
+}
